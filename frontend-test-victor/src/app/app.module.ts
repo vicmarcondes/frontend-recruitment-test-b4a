cbf8d55922f1c62643b0d0b87943b3cb9bf5e4dc
Victor Moreno <victor.marcondes@devmagic.com.br>
2023-03-28 14:25:50 -0300

    feat: posts carousel on homepage

--- frontend-test-victor/src/app/app.module.ts
+++ frontend-test-victor/src/app/app.module.ts
@@ -7,12 +7,14 @@ import { BannerComponent } from './home/banner/banner.component';
 import { CarouselModule } from 'ngx-owl-carousel-o';
 import { RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PostComponent } from './home/post/post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    BannerComponent
+    BannerComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
--- frontend-test-victor/src/app/home/home.component.ts
+++ frontend-test-victor/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   showToast: boolean = true;
+
+  customOptionsPosts: OwlOptions = {
+    center: true,
+    loop: true,
+    responsive: {
+      1000: {
+        items: 4,
+      }
+    }
+  }
+
+
+  posts: any = [
+    {
+      imageSrc: "../../../assets/images/post1.png",
+      username: "caroll_cassis",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et pretium sed dis eu pretium sapien nisl neque tincidunt."
+    },
+    {
+      imageSrc: "../../../assets/images/post2.png",
+      username: "bypalomasoares",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et pretium sed dis eu pretium sapien nisl neque tincidunt."
+    },
+    {
+      imageSrc: "../../../assets/images/post3.png",
+      username: "carolaine_moreira",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et pretium sed dis eu pretium sapien nisl neque tincidunt."
+    },
+    {
+      imageSrc: "../../../assets/images/post4.png",
+      username: "thaysnrezende",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et pretium sed dis eu pretium sapien nisl neque tincidunt."
+    },
+    {
+      imageSrc: "../../../assets/images/post5.png",
+      username: "laissaorsano",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et pretium sed dis eu pretium sapien nisl neque tincidunt."
+    },
+
+  ]
 
   constructor() { }
 
