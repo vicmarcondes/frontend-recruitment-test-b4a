612d537cdd360fc9e62c8747a81dc758562daccf
Victor Moreno <victor.marcondes@devmagic.com.br>
2023-03-30 11:31:30 -0300

    feat: create homepage service

--- frontend-test-victor/src/app/home/home.component.ts
+++ frontend-test-victor/src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { OwlOptions } from 'ngx-owl-carousel-o';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { HomepageService } from '../homepage.service';
 
 @Component({
   selector: 'app-home',
@@ -36,77 +37,9 @@ export class HomeComponent implements OnInit {
   }
 
 
-  posts: any = [
-    {
-      imageSrc: "../../../assets/images/post1.png",
-      username: "caroll_cassis",
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et pretium sed dis eu pretium sapien nisl neque tincidunt."
-    },
-    {
-      imageSrc: "../../../assets/images/post2.png",
-      username: "bypalomasoares",
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et pretium sed dis eu pretium sapien nisl neque tincidunt."
-    },
-    {
-      imageSrc: "../../../assets/images/post3.png",
-      username: "carolaine_moreira",
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et pretium sed dis eu pretium sapien nisl neque tincidunt."
-    },
-    {
-      imageSrc: "../../../assets/images/post4.png",
-      username: "thaysnrezende",
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et pretium sed dis eu pretium sapien nisl neque tincidunt."
-    },
-    {
-      imageSrc: "../../../assets/images/post5.png",
-      username: "laissaorsano",
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et pretium sed dis eu pretium sapien nisl neque tincidunt."
-    },
+  posts: any = [];
 
-  ]
-
-  products: any = [
-    {
-      title: "Kit Glam Beauty skincare rapidinho",
-      originalValue: "999,90",
-      currentValue: "999,80",
-      index: 0,
-      tag: "Proteção",
-      imageSrc: "../../../assets/images/product1.png"
-    },
-    {
-      title: "Máscara Glam detox de argila verde",
-      originalValue: "999,90",
-      currentValue: "999,80",
-      index: 1,
-      tag: "Hidratação",
-      imageSrc: "../../../assets/images/product2.png"
-    },
-    {
-      title: "Espuma de limpeza Glam Beauty cleam 150 ml",
-      originalValue: "999,90",
-      currentValue: "999,80",
-      index: 2,
-      tag: "Limpeza",
-      imageSrc: "../../../assets/images/product3.png"
-    },
-    {
-      title: "Hidratante com proteção solar 50 g",
-      originalValue: "999,90",
-      currentValue: "999,80",
-      index: 3,
-      tag: "Proteção",
-      imageSrc: "../../../assets/images/product4.png"
-    },
-    {
-      title: "Água micelar Glam Beauty fresh 200 ml",
-      originalValue: "999,90",
-      currentValue: "999,80",
-      index: 4,
-      tag: "Hidratação",
-      imageSrc: "../../../assets/images/product5.png"
-    },
-  ]
+  products: any = [];
 
   customOptionsProducts: OwlOptions = {
     center: true,
@@ -136,16 +69,21 @@ export class HomeComponent implements OnInit {
     name: new FormControl('', [Validators.required]),
     email: new FormControl('', [Validators.required, Validators.email]),
   });
+
   emailSuccess: boolean;
 
   constructor(
-    private snackbar: MatSnackBar
+    private snackbar: MatSnackBar,
+    private homepageService: HomepageService
   ) { }
 
   ngOnInit(): void {
     let toast = localStorage.getItem('showToast');
     
     this.showToast = toast === 'false' ? false : true;
+
+    this.products = this.homepageService.getProducts();
+    this.posts = this.homepageService.getPosts();
   }
 
   closeToast() {
